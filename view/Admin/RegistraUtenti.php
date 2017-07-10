2b90686887bc9b12adaa3794f1ea18ba4ed442f0
Michele <michelePap@users.noreply.github.com>
2017-07-10 13:58:08 +0200

    Update RegistraUtenti.php

--- view/Admin/RegistraUtenti.php
+++ view/Admin/RegistraUtenti.php
@@ -51,20 +51,12 @@
                     </a>
                     <ul class="dropdown-menu dropdown-user">
 						<?php
-<<<<<<< HEAD
-                        $hello = '
-						<li><a><i class="fa fa-user fa-fw"></i> Ciao, Admin ! </a>
-						';
-                        echo $hello;
-						
-=======
                         /*
                             Saluto utente dropdown list
                         */
                         $hello = '<li><a><i class="fa fa-user fa-fw">
                         </i> Ciao, Admin ! </a>';
                         echo hello;
->>>>>>> 7c95ad26bdd780c01e39c09ad046d8e5c98b9a23
 						?>
                         </li>
                         <li class="divider"></li>
