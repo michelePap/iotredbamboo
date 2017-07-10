3f248c9a935c38ae8e21d006e2fd904705d9cafa
Gianpr0 <gianlucamaietta@hotmail.it>
2017-07-10 13:07:47 +0200

    config.php moificato

--- controller/config.php
+++ controller/config.php
@@ -1,8 +1,7 @@
 <?php
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "iotredbamboo";
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = '';
+    $db_name = 'iotredbamboo';
     
     $connect = mysql_connect($db_host,$db_user,$db_password);
-?>
