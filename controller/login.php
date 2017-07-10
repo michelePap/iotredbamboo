5948a50c4dc6d06bda81944661855874f3553dff
Michele <mikele91.xeno@gmail.com>
2017-07-10 17:16:53 +0200

    commit

--- controller/login.php
+++ controller/login.php
@@ -12,7 +12,7 @@ mysql_select_db("$db_name",$connect);
 //$interrogazione = "SELECT * FROM `utenti` WHERE Username = '$username' AND Password = '$password'";
 
 $interrogazione = sprintf(
-  "SELECT * FROM `utenti` WHERE Username = '$username' AND Password = '$password'",
+  "SELECT * FROM `utenti` WHERE Username = '%s' AND Password = '%s'",
   mysql_real_escape_string($username),
   mysql_real_escape_string($password)
 );
