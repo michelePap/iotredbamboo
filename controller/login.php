57a707608fb4a6ea3e5049b388c6c09eb6e64802
Michele <mikele91.xeno@gmail.com>
2017-07-11 10:52:36 +0200

    commit

--- controller/login.php
+++ controller/login.php
@@ -1,33 +1,22 @@
 <?php
 session_start();
-include 'config.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $_SESSION['username'] = $username;
 
-mysql_select_db("$db_name",$connect);
+$dbh = new PDO("mysql:dbname=iotredbamboo;host=localhost", "root", "");
+$stmt = $dbh->prepare( 'SELECT * FROM `utenti` WHERE Username = :username AND Password = :password' );
+$stmt->execute( array(':username' => $username, ':password' => $password) );
+$risultato = $stmt->fetch(PDO::FETCH_NUM);
 
-//$interrogazione = "SELECT * FROM `utenti` WHERE Username = '$username' AND Password = '$password'";
-
-$interrogazione = sprintf(
-  "SELECT * FROM `utenti` WHERE Username = '%s' AND Password = '%s'",
-  mysql_real_escape_string($username),
-  mysql_real_escape_string($password)
-);
-
-$Risultato = mysql_query($interrogazione, $connect);
-
-$privilegi = mysql_result ($Risultato, $NumeroRighe, "Privilegi");
-$NumeroRighe = mysql_num_rows($Risultato);
-
-if($NumeroRighe==1 && $privilegi==1){
+if ($risultato[0] == "admin") {
 	header('location:../view/Admin/HomeAdmin.php');
-} else if($NumeroRighe==1 && $privilegi==0){
-	header('location:../view/Utente/HomeUtente.php');
-} else {
+
+} else if($risultato[0] == null) {
 	header('location:../view/ErrorLogin.html');
+
+} else {
+	header('location:../view/Utente/HomeUtente.php');
 }
-
-
