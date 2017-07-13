d918cdc778dd9ec5e1453e2b5f820ecc3e817cf3
Gianpr0 <gianlucamaietta@hotmail.it>
2017-07-13 09:50:32 +0200

    c

--- controller/RimuoviSensore.php
+++ controller/RimuoviSensore.php
@@ -9,13 +9,17 @@
 	$token = 'io';
 	$verifica= 'io';
 	
-		$dbh = new PDO('mysql:dbname=iotredbamboo;host=localhost', 'root', '');
 		
-		if ($token === $verifica){
+		
+		if (hash_equals($token, $verifica)){
+		$dbh = new PDO('mysql:dbname=iotredbamboo;host=localhost', 'root', '');
 		$stmt = $dbh->prepare( 'DELETE FROM `sensori` WHERE `sensori`.`IdSensori` = :IdSensore');
-		}
 		$stmt->bindParam(':IdSensore', $IdSensore);
 		$stmt->execute();
+} else {
+	$str = 'token non verificato'
+	echo $str;
+}
 
 	
 	  	header('location:../view/Admin/HomeAdmin.php');
