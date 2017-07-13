a20b401f800129399a4ad49f367ec4e7ae83a85e
Gianpr0 <gianlucamaietta@hotmail.it>
2017-07-13 09:43:47 +0200

    c

--- controller/RimuoviSensore.php
+++ controller/RimuoviSensore.php
@@ -9,16 +9,15 @@
 	$csrf = 'io';
 	$csrf1= 'io';
 	
-		if ($csrf === $csrf1){
-			
 		$dbh = new PDO('mysql:dbname=iotredbamboo;host=localhost', 'root', '');
-
+		
+		if ($csrf === $csrf1){
 		$stmt = $dbh->prepare( 'DELETE FROM `sensori` WHERE `sensori`.`IdSensori` = :IdSensore');
+		}
 		$stmt->bindParam(':IdSensore', $IdSensore);
 		$stmt->execute();
 
-	}else{
-		}	
+	
 	  	header('location:../view/Admin/HomeAdmin.php');
 
 	
